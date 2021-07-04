2c35a28a5432302b4bf92fdedd7ef3ecac4395d2
omarkk1 <omarasd16@gmail.com>
2021-07-04 18:11:26 +0300

    test

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('usersIndex', { title: 'Users Page' });
+  res.render('usersIndex', { title: 'index' });
 });
 
 module.exports = router;
